15550bd06a20a16d5a9f2fb1a77274d253952b5f
Abhishek Ahlawat <120219652+Abhishek18071999@users.noreply.github.com>
2023-07-04 20:52:47 +0530

    Add files via upload

---
+++ varName.java
@@ -0,0 +1,10 @@
+public class varName {
+    public static void main(String[] args) {
+        String name = "Abhishek Ahlawat";
+        int age = 23;
+        double height = 6.0;
+        System.out.println("Name : "+name);
+        System.out.println("Age : "+age);
+        System.out.println("Height : "+height);
+    }
+}
